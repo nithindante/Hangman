d7e8bec266bb296caaf7cfd33e00f72d64ec811c
nithindante <Rajkumar_nithin@yahoo.com>
2024-02-13 00:17:36 +0530

    Create a ruby file thatcreates a commmand line in Hangman game, computer thinks of a word, phrase, or sentence and the other(s) tries to guess it by suggesting letters or numbers within a certain number of guesses.

--- hangman.rb
+++ hangman.rb
@@ -1,66 +1,150 @@
-my_file = File.open('google-10000-english-no-swears.txt', 'r')
-computer_choice=[]
+require 'yaml'
+my_file = File.open('strings.txt', 'r')
+computer_choice = []
 
-def generate_random_word(computer_choice,my_file)
-  while line = my_file.gets 
-    if line.length > 5 && line.length <= 12
-      computer_choice.push(line.chomp)
-    end
+def generate_random_word(computer_choice, my_file)
+  while line = my_file.gets
+    computer_choice.push(line.chomp) if line.length > 5 && line.length <= 12
   end
-    return computer_choice.sample
+  computer_choice.sample
 end
 
-random_word = generate_random_word(computer_choice,my_file)
-p random_word
-no_of_guesses = 6
+random_word = generate_random_word(computer_choice, my_file)
+no_of_guesses = 10
 p "no of guesses is #{no_of_guesses}"
-user_choice = []
 
-def input_from_user()
-    p 'Enter your choice'
-    input_choice = gets.chomp.to_s
-    #user_choice.push(input_choice)
+def input_from_user
+  p 'Enter your choice'
+  input_choice = gets.chomp.to_s
 end
 
 arr_randomword = random_word.split('')
 
 arr = []
 len = arr_randomword.length
-for i in 0..len-1 do
+for i in 0..len - 1 do
   arr.push('_')
 end
 
-wrong_choices=[]
+wrong_choices = []
 
-def guess_game(arr_randomword, input_choice, arr, wrong_choices,no_of_guesses)
-    #p "my input is #{input_choice}"
-    unless arr_randomword.include?(input_choice)
-        unless wrong_choices.include?(input_choice)
-            wrong_choices.push(input_choice.chomp)
-            no_of_guesses -= 1
-            print "Your guess is wrong,"
-            p wrong_choices
-        else
-            print "Your guess is wrong,"
-            p wrong_choices
-        end
-    else
-        arr_randomword.each_with_index do |letter, index|
-            if input_choice.downcase == letter
-                arr[index] = letter
-                p "Your guess is correct,"
-                no_of_guesses -= 1
-                p arr
-                print "Wrong guess="
-                p wrong_choices
-            end
-        end
+def guess_game(arr_randomword, input_choice, arr, wrong_choices, no_of_guesses)
+  if arr_randomword.include?(input_choice.downcase)
+    arr_randomword.each_with_index do |letter, index|
+      next unless input_choice.downcase == letter
+
+      arr[index] = letter.downcase
+      p 'Your guess is correct,'
+      no_of_guesses -= 1
+      p arr
+      print 'Wrong guess='
+      p wrong_choices
     end
+  elsif wrong_choices.include?(input_choice)
+    no_of_guesses -= 1
+    print 'Your guess is wrong,'
+    p wrong_choices
+    print 'The correctly guessed elements are'
+    p arr
+  else
+    wrong_choices.push(input_choice.chomp)
+    no_of_guesses -= 1
+    print 'Your guess is wrong,'
+    p wrong_choices
+    print 'The correctly guessed elements are'
+    p arr
+  end
 end
- 
-for i in 1..no_of_guesses
-    input = input_from_user()
-    guess_game(arr_randomword,input,arr,wrong_choices,no_of_guesses)
-    p "no of guess remaining is #{no_of_guesses-i}"
 
+class GameStatus
+  attr_accessor :elements_in_correct, :no_of_guesses, :wrong_guesses
+
+  def initialize(elements_in_correct, no_of_guesses, wrong_guesses,random_array)
+    @elements_in_correct = elements_in_correct
+    @no_of_guesses = no_of_guesses
+    @wrong_guesses = wrong_guesses
+    @random_array = random_array
+  end
+
+  def to_yaml
+    YAML.dump({
+                elements_in_correct: @elements_in_correct,
+                no_of_guesses: @no_of_guesses,
+                wrong_guesses: @wrong_guesses,
+                random_array: @random_array
+              })
+  end
+
+  def self.from_yaml(string)
+    data = YAML.load string
+    p data
+    new(data[:elements_in_correct], data[:no_of_guesses], data[:wrong_guesses],data[:random_array])
+  end
+end
+
+def check_winner(arr, arr_randomword)
+  return 1 if arr == arr_randomword
+end
+sum = 10
+
+
+
+for i in 1..no_of_guesses
+  if i ==1
+    p 'Do you want to load the previous saved version (Y/N)'
+    saved_version = gets.chomp.to_s
+    if saved_version.downcase=="y"
+       demo = YAML.load(File.read("file.yaml"))
+        arr = demo[:elements_in_correct]
+        sum = demo[:no_of_guesses]
+        wrong_choices = demo[:wrong_guesses]
+        arr_randomword = demo[:random_array]
+    else
+      p 'Do you want to save the game(Y) or continue playing(N)'
+      choice = gets.chomp.to_s
+      if choice == 'Y'
+        demo = GameStatus.new arr, sum, wrong_choices,arr_randomword
+        File.open("file.yaml", "w") do |file|
+          file.write(demo.to_yaml)
+        end
+        print 'Save game'
+        next
+      else
+        sum = sum-1
+        input = input_from_user
+        guess_game(arr_randomword, input, arr, wrong_choices,no_of_guesses)
+        p "no of guess remaining is #{sum}"
+          if check_winner(arr, arr_randomword) == 1
+            p 'You guessed it right'
+            break
+          elsif no_of_guesses - i == 0
+            p 'Sorry, you werent able to guess the correct word'
+            break
+          end
+      end
+    end
+  else
+      p 'Do you want to save the game(Y) or continue playing(N)'
+      choice = gets.chomp.to_s
+      if choice == 'Y'
+        demo = GameStatus.new arr, sum, wrong_choices, arr_randomword
+        File.open("file.yaml", "w") do |file|
+          file.write(demo.to_yaml)
+        end
+        print 'Game Saved'
+        next
+      else
+        sum = sum-1
+        input = input_from_user
+        guess_game(arr_randomword, input, arr, wrong_choices, no_of_guesses)
+        p "no of guess remaining is #{sum}"
+        if check_winner(arr, arr_randomword) == 1
+          p 'You guessed it right'
+          break
+        elsif no_of_guesses - i == 0
+          p 'Sorry, you werent able to guess the correct word'
+          break
+        end
+      end
+  end
 end
